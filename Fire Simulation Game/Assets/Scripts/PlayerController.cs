2a3b377656ccc7982d47d7706644398c52116779
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-08-31 21:35:41 +0800

    Compartmentalized actions in script (crawling was also implemented in the last commit)

--- Fire Simulation Game/Assets/Scripts/PlayerController.cs
+++ Fire Simulation Game/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,11 @@ public class PlayerController : MonoBehaviour
     private Transform cameraTransform;
 
     private string currentState;
+
     [SerializeField] private float walkingSpeed;
     [SerializeField] private float crawlingSpeed;
     [SerializeField] private float runningSpeed;
+    [SerializeField] private float currentSpeed;
 
     private Vector3 previousMousePosition;
     [SerializeField] private float mouseSensitivity;
@@ -29,13 +31,12 @@ public class PlayerController : MonoBehaviour
         cameraTransform = transform.GetChild(0);
 
         currentState = "Walking";
+        SetupUprightState();
+        currentSpeed = walkingSpeed;
 
         previousMousePosition = Input.mousePosition;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
-
-        isVerticalTiltEnabled = true;
-        isHorizontalTiltEnabled = true;
 
         previousEulerAngles = Vector3.zero;
         rollingTimeLeft = 0.0f;
@@ -46,132 +47,61 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // basic controls
+        Move();
+        LookAround();
+
+        if (currentState.Equals("Rolling")) RollOver();
+        
+        else
+        {
+            // actions
+            if (Input.GetKeyDown(KeyCode.C)) ToggleCrawl();
+            else if (Input.GetKeyDown(KeyCode.F)) ToggleRun();
+            else if (Input.GetKeyDown(KeyCode.R)) InitiateRollOver();
+            if (Input.GetMouseButton(0)) CoverNose(); // holding of left click button
+        }
+
+        TestFunction(); // for debugging (i.e. Debug.Log)
+    }
+
+    void Move()
+    {
         Vector3 movementVector = Vector3.zero;
 
         // movement controls
         if (Input.GetKey(KeyCode.W))
+        {
             movementVector = new Vector3(transform.forward.x, 0.0f, transform.forward.z);
+        }
         else if (Input.GetKey(KeyCode.S))
+        {
             movementVector = new Vector3(-transform.forward.x, 0.0f, -transform.forward.z);
+        }
         
         if (Input.GetKey(KeyCode.A))
+        {
             movementVector += new Vector3(-transform.right.x, 0.0f, -transform.right.z);
+        }
         else if (Input.GetKey(KeyCode.D))
+        {
             movementVector += new Vector3(transform.right.x, 0.0f, transform.right.z);
+        }
+
+        // if the player attempts to move when they are currently rolling,
+        if (movementVector != Vector3.zero && currentState.Equals("Rolling"))
+        {
+            // switch them to crawling state
+            transform.eulerAngles = previousEulerAngles;
+            ToggleCrawl();
+        }
 
         // move character accordingly
-        if (currentState.Equals("Crawling"))
-            rb.velocity = movementVector.normalized * crawlingSpeed;
-        else if (currentState.Equals("Running"))
-            rb.velocity = movementVector.normalized * runningSpeed;
-        else
-            rb.velocity = movementVector.normalized * walkingSpeed;
+        rb.velocity = movementVector.normalized * currentSpeed;
+    }
 
-
-        // toggle for crawling
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            if (currentState.Equals("Crawling"))
-            {
-                currentState = "Walking";
-
-                isVerticalTiltEnabled = true;
-                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
-
-                cameraTransform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
-            }
-            else
-            {
-                currentState = "Crawling";
-
-                transform.position = new Vector3(transform.position.x, 0.63f, transform.position.z);
-                isVerticalTiltEnabled = false;
-                transform.eulerAngles = new Vector3(75, transform.eulerAngles.y, transform.eulerAngles.z);
-
-                cameraTransform.eulerAngles = new Vector3(30, transform.eulerAngles.y, transform.eulerAngles.z);
-            }
-        }
-
-
-        // toggle for running
-        else if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (currentState.Equals("Running"))
-                currentState = "Walking";
-            else
-                currentState = "Running";
-        }
-
-
-        // key press to start rolling over
-        else if (Input.GetKeyDown(KeyCode.R))
-        {
-            if (currentState.Equals("Rolling"))
-            {
-                currentState = "Crawling";
-                isHorizontalTiltEnabled = true;
-                transform.eulerAngles = previousEulerAngles;
-
-                cameraTransform.eulerAngles = new Vector3(30, transform.eulerAngles.y, transform.eulerAngles.z);
-            }
-            else
-            {
-                isVerticalTiltEnabled = false;
-                isHorizontalTiltEnabled = false;
-                rollingTimeLeft = 5.0f;
-                rotationCount = 37;
-                rollRotation = 5.0f;
-
-                transform.position = new Vector3(transform.position.x, 0.63f, transform.position.z);
-                transform.eulerAngles = new Vector3(75, transform.eulerAngles.y, transform.eulerAngles.z);
-                previousEulerAngles = transform.eulerAngles;
-
-                cameraTransform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-                
-                currentState = "Rolling";
-            }
-        }
-
-
-        if (currentState.Equals("Rolling"))
-        {
-            transform.Rotate(0.0f, rollRotation, 0.0f, Space.Self);
-            rotationCount--;
-
-            if (rotationCount == 0)
-            {
-                rotationCount = 74;
-                rollRotation = -rollRotation;
-            }
-
-            rollingTimeLeft -= Time.deltaTime;
-
-            if (rollingTimeLeft <= 0.0f)
-            {
-                currentState = "Crawling";
-                isHorizontalTiltEnabled = true;
-                transform.eulerAngles = previousEulerAngles;
-
-                cameraTransform.eulerAngles = new Vector3(30, transform.eulerAngles.y, transform.eulerAngles.z);
-            }
-        }
-
-
-        // holding of left click button
-        if (Input.GetMouseButton(0))
-        {
-            // if not holding anything, is covering nose
-            // else using item (when applicable)
-        }
-        
-
-        // key press for testing
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Debug.Log(transform.eulerAngles);
-        }
-
-
+    void LookAround()
+    {
         // adjusting camera when looking around
         Vector3 currentMousePosition = Input.mousePosition;
 
@@ -193,5 +123,105 @@ public class PlayerController : MonoBehaviour
             transform.Rotate(-(currentMousePosition.y - previousMousePosition.y) * mouseSensitivity, 0, 0, Space.Self);
 
         previousMousePosition = currentMousePosition;
+    }
+
+    void ToggleCrawl()
+    {
+        if (currentState.Equals("Crawling"))
+        {
+            currentState = "Walking";
+            SetupUprightState();
+            currentSpeed = walkingSpeed;
+        }
+        else
+        {
+            currentState = "Crawling";
+            SetupCrawlState();
+            currentSpeed = crawlingSpeed;
+        }
+    }
+
+    void ToggleRun()
+    {
+        if (currentState.Equals("Running"))
+        {
+            currentState = "Walking";
+            currentSpeed = walkingSpeed;
+        }
+        else
+        {
+            if (!currentState.Equals("Walking")) SetupUprightState();
+
+            currentState = "Running";
+            currentSpeed = runningSpeed;
+        }
+    }
+
+    void InitiateRollOver()
+    {
+        isVerticalTiltEnabled = false;
+        isHorizontalTiltEnabled = false;
+        rollingTimeLeft = 5.0f;
+        rotationCount = 37;
+        rollRotation = 5.0f;
+
+        transform.position = new Vector3(transform.position.x, 0.63f, transform.position.z);
+        transform.eulerAngles = new Vector3(75, transform.eulerAngles.y, transform.eulerAngles.z);
+        previousEulerAngles = transform.eulerAngles;
+        cameraTransform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        
+        currentSpeed = 0;
+        currentState = "Rolling";
+    }
+
+    void RollOver()
+    {
+        transform.Rotate(0.0f, rollRotation, 0.0f, Space.Self);
+        rotationCount--;
+
+        if (rotationCount == 0)
+        {
+            rotationCount = 74;
+            rollRotation = -rollRotation;
+        }
+
+        rollingTimeLeft -= Time.deltaTime;
+
+        if (rollingTimeLeft <= 0.0f)
+        {
+            transform.eulerAngles = previousEulerAngles;
+            ToggleCrawl();
+        }
+    }
+
+    void CoverNose()
+    {
+        Debug.Log("Covering nose");
+    }
+
+    void SetupUprightState()
+    {
+        isVerticalTiltEnabled = true;
+        isHorizontalTiltEnabled = true;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
+        cameraTransform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    void SetupCrawlState()
+    {
+        transform.position = new Vector3(transform.position.x, 0.63f, transform.position.z);
+        isVerticalTiltEnabled = false;
+        isHorizontalTiltEnabled = true;
+        transform.eulerAngles = new Vector3(75, transform.eulerAngles.y, transform.eulerAngles.z);
+        cameraTransform.eulerAngles = new Vector3(30, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    void TestFunction()
+    {
+        // key press for testing
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(previousEulerAngles);
+        }
     }
 }
