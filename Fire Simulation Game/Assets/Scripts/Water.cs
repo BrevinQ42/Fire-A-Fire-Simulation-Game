ff1b4afbbcf8a166b328814b1cbffec54d97e523
Kasumirre <105653241+Kasumirre@users.noreply.github.com>
2024-09-24 01:38:17 +0800

    Updated the water script

--- Fire Simulation Game/Assets/Scripts/Water.cs
+++ Fire Simulation Game/Assets/Scripts/Water.cs
@@ -7,18 +7,18 @@ public class Water : FireFightingObject
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(!collision.collider.CompareTag("Fire") || !collision.collider.GetComponent<Pail>())
+        if (!collision.collider.CompareTag("Fire") && !collision.collider.GetComponent<Pail>() && !collision.collider.CompareTag("Smoke"))
         {
             Destroy(gameObject);
             Debug.Log("Water hit " + collision.collider.name);
