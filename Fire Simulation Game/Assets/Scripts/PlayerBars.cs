7461c6f1d49d9ae7a9b3032a4da97f83535c02ab
Kasumirre <105653241+Kasumirre@users.noreply.github.com>
2024-09-23 18:17:47 +0800

    Implemented smoke HP.

--- Fire Simulation Game/Assets/Scripts/PlayerBars.cs
+++ Fire Simulation Game/Assets/Scripts/PlayerBars.cs
@@ -6,7 +6,9 @@ public class PlayerBars : MonoBehaviour
 {
     //Oxygen
     public float oxygen = 100f;
-    public float oxygenDamage = 10f;
+    public float oxygenDamage = 5f;
+    public int collisionCount = 0;
+    public bool isLosingOxygen = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,6 @@ public class PlayerBars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (oxygen <= 0f)
         {
             Debug.Log("Player has run out of oxygen!");
@@ -30,9 +31,30 @@ public class PlayerBars : MonoBehaviour
     {
         if (other.CompareTag("Smoke")) 
         {
+            collisionCount++;
 
-            Debug.Log("Player made contact with smoke!");
+            Debug.Log("Player made contact with smoke! Current collision count: " + collisionCount);
 
+            // Start the oxygen depletion coroutine if it hasn't already started
+            if (!isLosingOxygen)
+            {
+                StartCoroutine(OxygenDamageOverTime());
+            }
+
+        }
+    }
+
+    // Coroutine to handle oxygen damage over time
+    IEnumerator OxygenDamageOverTime()
+    {
+        isLosingOxygen = true;
+
+        while (oxygen > 0)
+        {
+            oxygen -= (oxygenDamage * collisionCount) * Time.deltaTime;
+            Debug.Log("Oxygen Level: " + oxygen);
+
+            yield return new WaitForSeconds(1f);
         }
     }
 }
