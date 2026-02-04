702c39e8b845ff1990fe2b6b544c124bf7e74720
Nate Brevin Que <nate.que@student.ateneo.edu>
2026-02-05 00:01:09 +0800

    Fixed bug where NPCs warp from one floor to another

--- Fire Simulation Game/Assets/Scripts/NPC/NPC.cs
+++ Fire Simulation Game/Assets/Scripts/NPC/NPC.cs
@@ -196,7 +196,7 @@ public class NPC : MonoBehaviour
 
     public bool hasReachedTarget()
     {
-        return agent.remainingDistance <= agent.stoppingDistance;
+        return agent.remainingDistance <= agent.stoppingDistance && Mathf.Abs(agent.destination.y - position.y) <= 3.0f;
     }
 
     public void SetStoppingDistance(float distance)
